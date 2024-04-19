c5f70367e74d6bb0c6ba84602df2feb93e4b535a
zeapling <121182225+zeapling@users.noreply.github.com>
2024-04-18 23:24:09 -0300

    Delete campCBMOL directory

--- campCBMOL/js/scriptChaves.js
+++
@@ -1,3 +0,0 @@
-let times1 = ["Almondegas Fervecentes"  ,"Couves Bruchuleantes" , "Repolhos Revoltaods", "Milhesima Colheita", 
-"Águas Sujas" , "Pedras de Forno" , "Olhos Ardentes" , "Sentido Sublimes"];
-
